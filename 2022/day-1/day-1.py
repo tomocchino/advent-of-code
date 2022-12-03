2a48f88d7c05e46395abd9b08488505e4a2b9ebe
Tom Occhino <tomocchino@gmail.com>
2022-12-02 21:57:06 -0500

    First python solution

--- 2022/day-1/day-1.py
+++ 2022/day-1/day-1.py
@@ -1,2 +1,12 @@
-input = open("day-1/sample.txt", "r")
-print(input.readline())
+input = open("day-1/input.txt", "r").read()
+
+calorie_sums = []
+
+for line in input.split("\n\n"):
+    inventory = line.split("\n")
+    calorie_sums.append(sum([int(str) for str in inventory]))
+
+calorie_sums.sort(reverse=True)
+
+print("Part 1: %d" % calorie_sums[0])
+print("Part 2: %d" % sum(calorie_sums[0:3]))
