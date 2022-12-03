4effb1679718698a08cd465c6f5c1486697b8c8b
Tom Occhino <tomocchino@gmail.com>
2022-12-03 11:28:23 -0500

    some cleanup / renaming

--- 2022/day-3/day-3.js
+++ 2022/day-3/day-3.js
@@ -1,8 +1,5 @@
 const input = require("fs").readFileSync(`${__dirname}/sample.txt`).toString();
-
-function sum(array) {
-  return array.reduce((a, b) => a + b, 0);
-}
+const lines = input.split("\n");
 
 const LOWER_START = "a".charCodeAt(0) - 1;
 const UPPER_START = "A".charCodeAt(0) - 27;
@@ -16,37 +13,43 @@ function decode(letter) {
   }
 }
 
-let duplicates = [];
-input.split("\n").forEach((rucksack) => {
-  let mid = Math.floor(rucksack.length / 2);
-  let set1 = new Set(rucksack.slice(0, mid));
-  let set2 = new Set(rucksack.slice(mid));
+function sum(array) {
+  return array.reduce((a, b) => a + b, 0);
+}
 
+// Part 1
+
+let part1Duplicates = [];
+lines.forEach((line) => {
+  let mid = Math.floor(line.length / 2);
+  let set1 = new Set(line.slice(0, mid));
+  let set2 = new Set(line.slice(mid));
   Array.from(set2).forEach((val) => {
     if (set1.has(val)) {
-      duplicates.push(decode(val));
+      part1Duplicates.push(decode(val));
     }
   });
 });
 
-console.log(`Part 1: ${sum(duplicates)}`);
+console.log(`Part 1: ${sum(part1Duplicates)}`);
+
+// Part 2
 
 let groups = [];
-let lines = input.split("\n");
 for (let ii = 0; ii < lines.length; ii += 3) {
   groups.push([lines[ii], lines[ii + 1], lines[ii + 2]]);
 }
 
-let duplicates2 = [];
+let part2Duplicates = [];
 groups.map((group) => {
   let set1 = new Set(group[0]);
   let set2 = new Set(group[1]);
   let set3 = new Set(group[2]);
   Array.from(set3).forEach((val) => {
     if (set1.has(val) && set2.has(val)) {
-      duplicates2.push(decode(val));
+      part2Duplicates.push(decode(val));
     }
   });
 });
 
-console.log(sum(duplicates2));
+console.log(`Part 2: ${sum(part2Duplicates)}`);
