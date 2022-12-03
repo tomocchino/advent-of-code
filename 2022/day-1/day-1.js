85d800db546cf501d4ab4664c49f04a7e99c1b91
Tom Occhino <tomocchino@gmail.com>
2022-12-02 21:56:05 -0500

    remove an unnecessary slice in my JS solution, and sort right with the map

--- 2022/day-1/day-1.js
+++ 2022/day-1/day-1.js
@@ -4,11 +4,12 @@ function sum(array) {
   return array.reduce((a, b) => a + b, 0);
 }
 
-let calorieSums = input.split("\n\n").map((inventory) => {
-  return sum(inventory.split("\n").map((str) => parseInt(str, 10)));
-});
-
-calorieSums.sort((a, b) => b - a).slice(0, 3);
+let calorieSums = input
+  .split("\n\n")
+  .map((inventory) => {
+    return sum(inventory.split("\n").map((str) => parseInt(str, 10)));
+  })
+  .sort((a, b) => b - a);
 
 console.log(`Part 1: ${calorieSums[0]}`);
 console.log(`Part 2: ${sum(calorieSums.slice(0, 3))}`);
