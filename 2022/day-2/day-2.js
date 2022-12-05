7338fe6e98f02f02a68ad59f3491f4fdb3443341
Tom Occhino <tomocchino@gmail.com>
2022-12-04 21:35:50 -0500

    Simplify day-2 slightly

--- 2022/day-2/day-2.js
+++ 2022/day-2/day-2.js
@@ -12,57 +12,47 @@ const OUTCOMES = {
   loss: 0,
 };
 
-function scoreRound(move, outcome) {
-  return MOVES[move] + OUTCOMES[outcome];
-}
-
 let partOneMoves = {
-  X: "rock",
-  Y: "paper",
-  Z: "scissors",
+  X: MOVES.rock,
+  Y: MOVES.paper,
+  Z: MOVES.scissors,
 };
 
 let partOneOutcomes = {
-  "A X": "draw",
-  "A Y": "win",
-  "A Z": "loss",
-  "B X": "loss",
-  "B Y": "draw",
-  "B Z": "win",
-  "C X": "win",
-  "C Y": "loss",
-  "C Z": "draw",
+  "A X": OUTCOMES.draw,
+  "A Y": OUTCOMES.win,
+  "A Z": OUTCOMES.loss,
+  "B X": OUTCOMES.loss,
+  "B Y": OUTCOMES.draw,
+  "B Z": OUTCOMES.win,
+  "C X": OUTCOMES.win,
+  "C Y": OUTCOMES.loss,
+  "C Z": OUTCOMES.draw,
 };
 
 let partTwoMoves = {
-  "A X": "scissors",
-  "A Y": "rock",
-  "A Z": "paper",
-  "B X": "rock",
-  "B Y": "paper",
-  "B Z": "scissors",
-  "C X": "paper",
-  "C Y": "scissors",
-  "C Z": "rock",
+  "A X": MOVES.scissors,
+  "A Y": MOVES.rock,
+  "A Z": MOVES.paper,
+  "B X": MOVES.rock,
+  "B Y": MOVES.paper,
+  "B Z": MOVES.scissors,
+  "C X": MOVES.paper,
+  "C Y": MOVES.scissors,
+  "C Z": MOVES.rock,
 };
 
 let partTwoOutcomes = {
-  X: "loss",
-  Y: "draw",
-  Z: "win",
+  X: OUTCOMES.loss,
+  Y: OUTCOMES.draw,
+  Z: OUTCOMES.win,
 };
 
 let partOneSum = 0;
 let partTwoSum = 0;
 input.split("\n").forEach((round) => {
-  partOneSum += scoreRound(
-    partOneMoves[round.slice(-1)],
-    partOneOutcomes[round]
-  );
-  partTwoSum += scoreRound(
-    partTwoMoves[round],
-    partTwoOutcomes[round.slice(-1)]
-  );
+  partOneSum += partOneMoves[round.slice(-1)] + partOneOutcomes[round];
+  partTwoSum += partTwoMoves[round] + partTwoOutcomes[round.slice(-1)];
 });
 
 console.log(`Part 1: ${partOneSum}`);
