de212b621f8fca84e0a4ccb5b66d2a56ed1292c4
Tom Occhino <tomocchino@gmail.com>
2022-12-05 01:33:26 -0500

    Switch from using arrays for the stacks to strings, though it's unclear if this is actually any better…

--- 2022/day05/day05.js
+++ 2022/day05/day05.js
@@ -9,47 +9,35 @@ const moves = moveStrings.split("\n").map((str) => {
   return { num, from: from - 1, to: to - 1 };
 });
 
-function getStacks(diagram) {
-  let stacks = [];
-  let rows = diagram.split("\n");
-  let numCols = (rows[0].length + 1) / 4;
-  for (ii = 0; ii < numCols; ii++) {
-    stacks[ii] = [];
+let rows = diagram.split("\n");
+let numCols = (rows[0].length + 1) / 4;
+let stacks = new Array(numCols).fill("");
+rows.pop(); // pop the stack indices off…
+rows.forEach((row) => {
+  for (let ii = 0; ii < numCols; ii++) {
+    let offset = ii * 4 + 1;
+    let char = row[offset];
+    if (char != " ") {
+      stacks[ii] += char;
+    }
   }
+});
 
-  rows.pop(); // pop the stack indices off…
-  rows.forEach((row) => {
-    for (let ii = 0; ii < numCols; ii++) {
-      let offset = ii * 4 + 1;
-      let char = row[offset];
-      if (char != " ") {
-        stacks[ii].push(char);
-      }
-    }
-  });
-  return stacks;
-}
+let part1Stacks = stacks.slice();
+moves.forEach((move) => {
+  let { num, from, to } = move;
+  while (num--) {
+    part1Stacks[to] = part1Stacks[from][0] + part1Stacks[to];
+    part1Stacks[from] = part1Stacks[from].slice(1);
+  }
+});
 
-// Part 1
-{
-  let stacks = getStacks(diagram);
-  moves.forEach((move) => {
-    let { num, from, to } = move;
-    while (num--) {
-      stacks[to].unshift(stacks[from].shift());
-    }
-  });
+let part2Stacks = stacks.slice();
+moves.forEach((move) => {
+  let { num, from, to } = move;
+  part2Stacks[to] = part2Stacks[from].slice(0, num) + part2Stacks[to];
+  part2Stacks[from] = part2Stacks[from].slice(num);
+});
 
-  console.log(`Part 1: ${stacks.map((stack) => stack[0]).join("")}`);
-}
-
-// Part 2
-{
-  let stacks = getStacks(diagram);
-  moves.forEach((move) => {
-    let { num, from, to } = move;
-    stacks[to].unshift(...stacks[from].splice(0, num));
-  });
-
-  console.log(`Part 2: ${stacks.map((stack) => stack[0]).join("")}`);
-}
+console.log(`Part 1: ${part1Stacks.map((stack) => stack[0]).join("")}`);
+console.log(`Part 2: ${part2Stacks.map((stack) => stack[0]).join("")}`);
