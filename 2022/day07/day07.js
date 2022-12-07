b83db5cf7268970ee3ad23d9d5e51c03a024daf1
Tom Occhino <tomocchino@gmail.com>
2022-12-07 15:07:43 -0500

    Clean up day07 a little

--- 2022/day07/day07.js
+++ 2022/day07/day07.js
@@ -1,77 +1,63 @@
 const input = require("fs").readFileSync(`${__dirname}/input.txt`).toString();
 const lines = input.split("\n");
 
-function goOut(path) {
-  return path.split("/").slice(0, -1).join("/") || "/";
-}
-
-let cwd = "/";
-let node = { folders: [], size: 0 };
-let directories = {};
+let path = "/";
+let filesystem = {};
+let currentDirectory = null;
 lines.forEach((line) => {
-  if (line.startsWith("$ cd")) {
-    let dir = line.split(" ").pop();
-    if (dir === "..") {
-      cwd = goOut(cwd);
-      return;
-    } else if (dir === "/") {
-      cwd = "/";
-    } else if (cwd === "/") {
-      cwd = "/" + dir;
-    } else {
-      cwd += "/" + dir;
-    }
-    node = { folders: [], files: {}, size: 0 };
-    directories[cwd] = node;
-  } else if (line.match(/^\d+/)) {
-    let [size, fileName] = line.split(" ");
-    node.files[fileName] = Number(size);
-    node.size += Number(size);
+  if (line.match(/^\d+/)) {
+    let fileSize = Number(line.split(" ")[0]);
+    currentDirectory.size += fileSize;
   } else if (line.match(/^dir/)) {
-    let dir = line.split(" ")[1];
-    let folderName = cwd == "/" ? "/" + dir : cwd + "/" + dir;
-    node.folders.push(folderName);
+    let dirName = line.split(" ").pop();
+    currentDirectory.folders.push(path + "/" + dirName);
+  } else if (line.startsWith("$ cd")) {
+    let dirName = line.split(" ").pop();
+    if (dirName === "..") {
+      path = path.split("/").slice(0, -1).join("/");
+      return;
+    } else if (dirName != "/") {
+      path += "/" + dirName;
+    }
+
+    currentDirectory = { size: 0, folders: [] };
+    filesystem[path] = currentDirectory;
   }
 });
 
-let sizes = {};
-for (let name in directories) {
-  let directory = directories[name];
+let directorySizes = {};
+Object.keys(filesystem).forEach((path) => {
+  let directory = filesystem[path];
   let dirSize = directory.size;
   let folders = directory.folders;
   while (folders.length > 0) {
-    let nextDir = directories[folders.pop()];
+    let nextDir = filesystem[folders.pop()];
     dirSize += nextDir.size;
     if (nextDir.folders.length) {
       folders.push(...nextDir.folders);
     }
   }
-  sizes[name] = dirSize;
-}
-
-let totalSize = 0;
-Object.values(sizes).forEach((size) => {
-  if (size <= 100000) {
-    totalSize += size;
-  }
+  directorySizes[path] = dirSize;
 });
 
-console.log(`Part 1: ${totalSize}`); // Part 1: 1667443
+const usedDiskSpace = directorySizes["/"];
+const unusedDiskSpace = 70000000 - usedDiskSpace;
+const minDeletionSize = 30000000 - unusedDiskSpace;
 
-const totalDiskSpace = 70000000;
-const usedDiskSpace = sizes["/"];
-const unusedDiskSpaceNeeded = 30000000;
-const unusedDiskSpace = totalDiskSpace - usedDiskSpace;
-const minDeletionSize = unusedDiskSpaceNeeded - unusedDiskSpace;
+let partOneSize = 0;
+let partTwoSize = 0;
 
-let sortedSizes = Object.values(sizes).sort((a, b) => {
-  return a - b;
-});
+Object.values(directorySizes)
+  .sort((a, b) => {
+    return a - b;
+  })
+  .forEach((size) => {
+    if (size <= 100000) {
+      partOneSize += size;
+    } else if (!partTwoSize && size > minDeletionSize) {
+      partTwoSize = size;
+    }
+  });
 
-for (let ii = 0; ii < sortedSizes.length; ii++) {
-  let size = sortedSizes[ii];
-  if (size > minDeletionSize) {
-    console.log(`Part 2: ${size}`); // Part 2: 8998590
-    break;
-  }
-}
+console.log(`Part 1: ${partOneSize}`); // Part 1: 1667443
+console.log(`Part 2: ${partTwoSize}`); // Part 2: 8998590
