ae0a16f3140d826e1ba04a84f4b811512cbd58ad
Tom Occhino <tomocchino@gmail.com>
2022-12-08 22:38:20 -0500

    Some more day08 cleanup

--- 2022/day08/day08.js
+++ 2022/day08/day08.js
@@ -1,56 +1,42 @@
 const input = require("fs").readFileSync(`${__dirname}/input.txt`).toString();
 const lines = input.split("\n");
 
+let grid = lines.join("");
 let numRows = lines.length;
 let numCols = lines[0].length;
-let gridString = lines.join("");
 
-function getPoint(row, col) {
-  return gridString[row * numCols + col];
+function getTree(row, col) {
+  return Number(grid[row * numCols + col]);
 }
 
-function getDirectionalTreeInfo(row, col, direction) {
-  let point = getPoint(row, col);
-  let isClear = true;
+function getTreeInfo(row, col, direction) {
   let score = 0;
-  switch (direction) {
-    case "up":
-      while (isClear && --row >= 0) {
-        let next = getPoint(row, col);
-        if (next >= point) {
-          isClear = false;
-        }
-        score++;
-      }
-      break;
-    case "right":
-      while (isClear && ++col < numCols) {
-        let next = getPoint(row, col);
-        if (next >= point) {
-          isClear = false;
-        }
-        score++;
-      }
-      break;
-    case "down":
-      while (isClear && ++row < numRows) {
-        let next = getPoint(row, col);
-        if (next >= point) {
-          isClear = false;
-        }
-        score++;
-      }
-      break;
-    case "left":
-      while (isClear && --col >= 0) {
-        let next = getPoint(row, col);
-        if (next >= point) {
-          isClear = false;
-        }
-        score++;
-      }
-      break;
+  let isClear = true;
+  let currentTree = getTree(row, col);
+
+  function checkTree(row, col) {
+    isClear = getTree(row, col) < currentTree;
+    score++;
   }
+
+  if (direction == "n") {
+    while (isClear && --row >= 0) {
+      checkTree(row, col);
+    }
+  } else if (direction == "e") {
+    while (isClear && ++col < numCols) {
+      checkTree(row, col);
+    }
+  } else if (direction == "s") {
+    while (isClear && ++row < numRows) {
+      checkTree(row, col);
+    }
+  } else if (direction == "w") {
+    while (isClear && --col >= 0) {
+      checkTree(row, col);
+    }
+  }
+
   return { isClear, score };
 }
 
@@ -58,25 +44,17 @@ let visibleTrees = 0;
 let scenicScores = [];
 for (let row = 0; row < numRows; row++) {
   for (let col = 0; col < numCols; col++) {
-    let up = getDirectionalTreeInfo(row, col, "up");
-    let right = getDirectionalTreeInfo(row, col, "right");
-    let down = getDirectionalTreeInfo(row, col, "down");
-    let left = getDirectionalTreeInfo(row, col, "left");
+    let n = getTreeInfo(row, col, "n");
+    let e = getTreeInfo(row, col, "e");
+    let s = getTreeInfo(row, col, "s");
+    let w = getTreeInfo(row, col, "w");
 
-    if (up.isClear || right.isClear || down.isClear || left.isClear) {
+    if (n.isClear || e.isClear || s.isClear || w.isClear) {
       visibleTrees++;
     }
-    scenicScores.push(up.score * right.score * down.score * left.score);
+    scenicScores.push(n.score * e.score * s.score * w.score);
   }
 }
 
-console.log(`\nPart 1: ${visibleTrees}`);
+console.log(`Part 1: ${visibleTrees}`);
 console.log(`Part 2: ${Math.max(...scenicScores)}`);
-
-// Sample
-// Part 1: 21
-// Part 2: 8
-
-// Input
-// Part 1: 1538
-// Part 2: 496125
